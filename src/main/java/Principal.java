e2a13f49ed021f6df4a27476c0d70de9fd22a690
Eduardo Gonçalves <eduardouc388@gmail.com>
2026-02-05 22:35:24 -0300

    Update

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,5 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
 
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco endereco = consultaCep.buscaEndereco("01001");
+        System.out.println(endereco);
     }
 }
